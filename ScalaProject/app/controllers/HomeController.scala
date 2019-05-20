2386be2f492acb43deabed95bc52febec78f32db
manalito <aurelien.siu@heig-vd.ch>
2019-05-21 00:38:52 +0200

    added missing files for react, now react works on localhost:3000 with proxy on localhost:9000

--- ScalaProject/app/controllers/HomeController.scala
+++ ScalaProject/app/controllers/HomeController.scala
@@ -4,7 +4,6 @@ import dao.UsersDAO
 import javax.inject._
 import play.api.libs.json.Json
 import play.api.mvc._
-import play.api.routing.JavaScriptReverseRouter
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -15,22 +14,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, usersDAO: UsersDAO) extends AbstractController(cc) {
 
-  val title = "WaliDB 2019 !"
-
-  /**
-    * Create an Action to render an HTML page with a welcome message.
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
-  def index = Action.async { implicit request =>
-    val usersList = usersDAO.list()
-
-    // Wait for the promises to resolve, then return the list of students and courses.
-    for {
-      users <- usersList
-    } yield Ok(views.html.index(title, users))
-  }
 
   def appSummary = Action {
     Ok(Json.obj("content" -> "Scala Play React Seed"))
--- ScalaProject/build.sbt
+++ ScalaProject/build.sbt
@@ -1,14 +1,16 @@
+import sbt.Resolver
+
 name := "ScalaProject"
  
 version := "1.0" 
       
-lazy val `testplay2611` = (project in file(".")).enablePlugins(PlayScala)
+lazy val root = (project in file(".")).enablePlugins(PlayScala).settings(
+   watchSources ++= (baseDirectory.value / "public/ui" ** "*").get
+)
 
-resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
+resolvers += Resolver.sonatypeRepo("snapshots")
       
-resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
-      
-scalaVersion := "2.12.2"
+scalaVersion := "2.12.8"
 
 libraryDependencies ++= Seq( jdbc , ehcache , ws , specs2 % Test , guice )
 
