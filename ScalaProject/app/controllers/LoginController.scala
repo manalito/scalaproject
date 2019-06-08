d7e719676839ec043e10b30b9a13b55482abf8c8
romaingallay <romain@dhcp-10-192-115-27.einet.ad.eivd.ch>
2019-06-08 15:37:51 +0200

    add login

--- ScalaProject/app/controllers/LoginController.scala
+++ ScalaProject/app/controllers/LoginController.scala
@@ -1,10 +1,33 @@
 package controllers
 
-class LoginController {
+import dao.UsersDAO
+import javax.inject._
+import play.api.Logger
+import play.api.libs.json.{JsResult, JsSuccess, JsValue, Json}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Cookie}
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-  def login(user: String, password: String) = {
+@Singleton
+class LoginController @Inject()(cc: ControllerComponents, usersDAO: UsersDAO) extends AbstractController(cc) {
 
-  }
+    def login(): Action[JsValue] = Action.async(parse.json) {
+      request =>
+        val username = (request.body \ "username").as[String]
+        val password = (request.body \ "password").as[String]
 
+        usersDAO.authenticate(username,password).map {
+          case _ => Ok.withCookies(Cookie("walidb", username))
+          case false => NotFound
+        }
+        //Future {Ok(Json.obj("username" -> username, "pw" -> password))}
+    }
+/*
+  def login() = Action.async(parse.json) {
+      request =>
+      val logger: play.api.Logger = Logger(this.getClass)
+      logger.error("aaaaaaaaaaaaaaaaaaaaaaah")
+      Future {Ok(Json.obj("walidb" -> "iscool"))}
+  }*/
 }
